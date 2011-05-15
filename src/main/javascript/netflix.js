23016f5d3cb8a8a83515782abcc9493297890716
Justin Searls <searls@gmail.com>
2011-05-15 13:31:27 -0400

    extracted 4 private methods that explain what the first epic one-liner was accomplishing, which could later be extracted and reused elsewise. covers the functionality from tag `1`

--- src/main/javascript/netflix.js
+++ src/main/javascript/netflix.js
@@ -4,20 +4,36 @@
 (function($){
   window.netflix = {
     sortQueue: function() {
-      $.each(
-          $('#qbody tr a.mdpLink').contents().toArray().sort(
-            function(a,b){ 
-              return a.nodeValue > b.nodeValue ? 1 : -1; 
-            }
-          ),
-          function(i,e){ 
-            $(this).closest('tr').find('input.o').val(i+1).closest('tr').appendTo('#qbody')
-          }
-        );
+      alphabetizeTitles();
       $('<span class="reminder"><--  Now remember to click update!</span>').insertBefore('input[value="Update Instant Queue"]').css({'line-height':'1.3','font-size':'20px','float':'right','background-color':'red',color:'white','margin-left':'20px',display:'none'}).fadeIn(1400)
       $('html, body').animate({
         scrollTop: $(".reminder").offset().top - 10
       }, 200);      
     }
   };
+  
+  var alphabetizeTitles = function() {
+    $.each(sortTitlesInAlphaOrder(),applyOrder);
+  };
+  
+  var sortTitlesInAlphaOrder = function() {
+    return $('#qbody tr a.mdpLink').contents().toArray().sort(lexicalNodeComparator);
+  };
+  
+  var applyOrder = function(index,textNode) {
+    var $row = $(textNode).closest('tr');
+    $row.find('input.o').val(index+1);
+    $row.appendTo('#qbody');
+  };
+  
+  var lexicalNodeComparator = function(left,right){ 
+    var LEFT_IS_GREATER = 1,
+        RIGHT_IS_GREATER = -1;
+    if($.trim($(left).text()) > $.trim($(right).text())) {
+      return LEFT_IS_GREATER;
+    } else {
+      return RIGHT_IS_GREATER;
+    } 
+  };
+  
 })(jQuery)
