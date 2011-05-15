6a554bf6ae289f3faf09f73be3c6fb2be1d89f31
Justin Searls <searls@gmail.com>
2011-05-15 09:53:28 -0400

    Initial commit - netflix sort a-to-z

---
+++ src/main/javascript/netflix.js
@@ -0,0 +1,15 @@
+/* Types in the alphabetical order of a user's netflix queue into the little input boxes
+*  on the left side of each row. 
+*****/
+(function($){
+  $.each(
+      $('#qbody tr a.mdpLink').contents().toArray().sort(
+        function(a,b){ 
+          return a.nodeValue > b.nodeValue ? 1 : -1; 
+        }
+      ),
+      function(i,e){ 
+        $(this).closest('tr').find('input.o').val(i) 
+      }
+    );
+})(jQuery)
